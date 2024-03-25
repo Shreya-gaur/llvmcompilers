95b20cd30f76d6d2c61939c329889a54aed3b4b0
Shreya Gaur <gaur13@data.cs.purdue.edu>
2024-03-24 21:26:20 -0400

    part2 6 test cases passing

--- uscc-PA4/opt/RedundantPhi.cpp
+++ uscc-PA4/opt/RedundantPhi.cpp
@@ -14,8 +14,17 @@ class RedundantPhiRemoval : public FunctionPass {
 public:
   static char ID;
   RedundantPhiRemoval() : FunctionPass(ID) {}
+   
+  Graph* inducedSubGraph = new Graph();
+ 
+  std::vector<Value*> phiFunctions;
+  std::vector<std::set<llvm::Value *>> sccsSet;
+
   virtual bool runOnFunction(llvm::Function &F) override;
   void getAnalysisUsage(llvm::AnalysisUsage &AU) const override;
+  void processSCC(std::set<Value*> scc);
+  void removeRedundantPhis(std::vector<Value*>);
+  void replaceSCCByValue(std::set<Value*> scc, Value* value);
 };
 
 
@@ -34,11 +43,86 @@ bool RedundantPhiRemoval::runOnFunction(Function &F) {
   // This is the entry point of the redudant phi removal pass.
   // Your implementation should start here.
   // Feel free to add functions to the RedundantPhiRemoval class.
-  
 
-  return true;
+    if (F.empty())
+        return false;
+  
+	for (auto &bb : F){
+		for(auto &inst : bb){
+			auto phiNode = dyn_cast_or_null<llvm::PHINode>(&inst); 
+			if(phiNode)
+				phiFunctions.push_back(phiNode);		
+		}
+	}
+	
+	removeRedundantPhis(phiFunctions);
+
+    return true;
 }
 
+void RedundantPhiRemoval::removeRedundantPhis(std::vector<Value*> phiFunctions){
+
+	inducedSubGraph->eraseAllNodes();
+	std::vector<std::set<Value*>> sccs;
+	for(auto it = phiFunctions.begin(); it != phiFunctions.end(); ++it){
+		auto node = inducedSubGraph->getOrAddToGraph(*it); 
+
+		auto phiNode = dyn_cast_or_null<PHINode>(*it);
+		unsigned numOfIncomingValues = phiNode->getNumIncomingValues();
+		for(unsigned i=0; i<numOfIncomingValues; i++){
+			auto operand = phiNode->getIncomingValue(i);
+			if(dyn_cast_or_null<PHINode>(operand)){
+				auto operandNode = inducedSubGraph->getOrAddToGraph(operand);
+				node->addEdge(operandNode);
+			}
+		}
+	}
+
+    sccs = inducedSubGraph->getSCCs(); 
+	for(auto scc = sccs.begin(); scc != sccs.end(); ++scc){
+		processSCC(*scc);
+	}
+
+}
+
+void RedundantPhiRemoval::processSCC(std::set<Value*> scc){
+	
+	std::vector<Value*> inner;
+	std::vector<Value*> outerOps;
+		
+	for(auto phi = scc.begin(); phi !=scc.end(); ++phi){
+		bool isInner = true;
+		auto phiNode = dyn_cast_or_null<PHINode>(*phi);
+		unsigned numOfIncomingValues = phiNode->getNumIncomingValues();
+		for(unsigned i=0; i<numOfIncomingValues; i++){
+			auto operand = phiNode->getIncomingValue(i);
+			if(scc.find(operand) == scc.end()){
+				outerOps.push_back(operand);
+				isInner = false;
+			}
+		}
+		if(isInner)
+			inner.push_back(*phi);
+	}
+
+	if(outerOps.size() == 1){
+		replaceSCCByValue(scc, outerOps[outerOps.size() - 1]);
+	}
+	else if(outerOps.size() > 1){
+		removeRedundantPhis(inner);
+	}
+
+}
+
+void RedundantPhiRemoval::replaceSCCByValue(std::set<Value*> scc, Value* value){
+	
+	for(auto phi = scc.begin(); phi != scc.end(); ++phi){
+		auto phiNode = dyn_cast_or_null<PHINode>(*phi);
+		phiNode->replaceAllUsesWith(value);
+		phiNode->eraseFromParent();
+		inducedSubGraph->eraseFromGraph(*phi);
+	}
+}
 
 } // namespace opt
 } // namespace uscc
